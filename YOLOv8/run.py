2266c93944b97d2b05e39c21d95868a3fca59a52
pimang62 <pimang62@gmail.com>
2024-01-23 17:29:37 +0900

    bees

--- YOLOv8/run.py
+++ YOLOv8/run.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+# model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco128.yaml", epochs=3)  # train the model
+model.train(data="bees.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+results = model("https://images.labelon.kr/2022/11/10/a8705ce21cf94d4e8410082eb29e65cf.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
